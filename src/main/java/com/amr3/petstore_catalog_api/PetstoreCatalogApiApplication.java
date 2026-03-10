92193fcb75e28478ada03f0191b519cff12e05c4
Adrián Meneses Ramos <amr.academics@gmail.com>
2026-03-10 19:28:21 +0000

    chore: initialize Spring Boot 3 project with Maven and core dependencies

---
+++ src/main/java/com/amr3/petstore_catalog_api/PetstoreCatalogApiApplication.java
@@ -0,0 +1,13 @@
+package com.amr3.petstore_catalog_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetstoreCatalogApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetstoreCatalogApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amr3/petstore_catalog_api/PetstoreCatalogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amr3.petstore_catalog_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetstoreCatalogApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
